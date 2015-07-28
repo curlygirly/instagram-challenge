ac4c2f221e5371c18c803fb2ee41a96d5ca9f384
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-07-28 16:28:24 +0100

    Adds link and form to add new photo

--- avogram/app/controllers/photos_controller.rb
+++ avogram/app/controllers/photos_controller.rb
@@ -1,6 +1,19 @@
 class PhotosController < ApplicationController
 
+  def photo_params
+    params.require(:photo).permit(:name)
+    end
+
   def index
     @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    Photo.create(photo_params)
+    redirect_to '/photos'
   end
 end
--- avogram/spec/features/photos_features_spec.rb
+++ avogram/spec/features/photos_features_spec.rb
@@ -11,13 +11,24 @@ feature 'photos' do
 
   context 'photos have been added' do
     before do
-      Photo.create(name: 'avotoast')
+      Photo.create(name: 'Avotoast')
     end
 
     scenario 'display photos' do
       visit '/photos'
-      expect(page).to have_content('avotoast')
+      expect(page).to have_content('Avotoast')
       expect(page).not_to have_content('No photos yet')
+    end
+  end
+
+  context 'creating photos' do
+    scenario 'prompts user to fill out a form, then displays the new photo' do
+      visit '/photos'
+      click_link 'Add a photo'
+      fill_in 'Name', with: 'Avotoast'
+      click_button 'Create Photo'
+      expect(page).to have_content 'Avotoast'
+      expect(current_path).to eq '/photos'
     end
   end
 end
